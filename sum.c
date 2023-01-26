1b2fac07db9870491a8fa6a2d1b62614dea90cc7
ayush-9918 <ayushchauhan9918@gmail.com>
2023-01-26 21:25:00 +0530

    summing 3 numbers

--- sum.c
+++ sum.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
-int sum(int a, int b){
-    return a + b;
+int sum(int a, int b, int c){
+    return a + b + c;
 }
 int main(int argc, char* argv[]){
     int a = atoi(argv[1]);
     int b = atoi(argv[2]);
-    printf("%d\n", sum(a, b));
+    int c = atoi(argv[3]);
+    printf("%d\n", sum(a, b, c));
     return 0;
 }
